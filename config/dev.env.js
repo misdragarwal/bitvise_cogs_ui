73307325ab72724c8180700821bf383ed357c1f4
misdragarwal <68944154+misdragarwal@users.noreply.github.com>
2023-03-10 11:16:14 +0530

    DRT API URL changes

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,9 +4,9 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-//  API_URL:'"https://mis.dragarwal.com"',
+  API_URL:'"https://mis.dragarwal.com"',
  // API_URL:'"http://localhost:8888"',
-  API_URL:'"http://testmis.dragarwal.com"',
+//  API_URL:'"http://testmis.dragarwal.com"',
 
  API_KEY:'"730d84a872b5987b66d7b3b91cd8b8ca7c3c4fdcfd00022fd4d32f1f142f072fa505235a06821746edcbe430fcd9d21ce78025c5ebe647bebd6cef207a7cae2b"'
 })
